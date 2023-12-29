33ceccb36d07941fde38cd02ca1d082bd870f042
Ruban Dzmitry <ruban251219872023@gmail.com>
2023-12-29 23:07:55 +0300

    add js

---
+++ doc.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+   let element = document.getElementById('demo');
+   if (element.style.display === 'none') {
+      element.style.display = 'block';
+   } else {
+      element.style.display = 'none';
+   }
+}
