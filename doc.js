2792c78574dd207fc225b63cf9fc31ff31781774
Ruban Dzmitry <ruban251219872023@gmail.com>
2023-12-29 23:26:21 +0300

    add experience in header

--- doc.js
+++ doc.js
@@ -7,3 +7,31 @@ function myFunction() {
       element.style.display = 'none';
    }
 }
+
+function myFunctionSandoz() {
+   let element = document.getElementById('demo2');
+   if (element.style.display === 'none') {
+      element.style.display = 'block';
+   } else {
+      element.style.display = 'none';
+   }
+}
+
+function myFunctionArterium() {
+   let element = document.getElementById('demo3');
+   if (element.style.display === 'none') {
+      element.style.display = 'block';
+   } else {
+      element.style.display = 'none';
+   }
+}
+
+function myFunctionMorgue() {
+   let element = document.getElementById('demo4');
+   if (element.style.display === 'none') {
+      element.style.display = 'block';
+   } else {
+      element.style.display = 'none';
+   }
+}
+
